5aa602ddb278bf5e8c8cc248f630bf5a3a5a8a5c
bradham <bradham@users.noreply.github.com>
2019-09-02 22:32:26 -0500

    Button click adds 10

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -9,7 +9,7 @@ input to add to array
 button submit to add to array
  */
 
-var topics = ["Boba Fett", "Luke", "Chewbacca"];
+var topics = ["Boba Fett", "Luke", "Chewbacca", "Rey", "speeder"];
 
 
 function renderButtons() {
@@ -35,34 +35,71 @@ function renderButtons() {
 }
 
 // Function for dumping the JSON content for each button into the div
+//TO DO: overwrite/refresh results displayed after button is clicked
 function displayTopicInfo() {
 
-var topic = $(this).attr("data-name");
-var queryURL = "https://api.giphy.com/v1/gifs/random?apikey=Ab6N6BpDEAK7ZXDlD0R2FwxpnJeZFKOK&tag=starwars"
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?apikey=Ab6N6BpDEAK7ZXDlD0R2FwxpnJeZFKOK&q=starwars+" + topic + "&limit=10";
 
-//Example from omdb api
-// "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+    //Example from omdb api
+    // "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-          // Saving the image_original_url property
-          var imageUrl = response.data.image_original_url;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.data;
 
-          // Creating and storing an image tag
-          var giph = $("<img>");
+        // Looping over every result item
+        for (var i = 0; i < results.length; i++) {
 
-          // Setting the giph src attribute to imageUrl
-          giph.attr("src", imageUrl);
-          giph.attr("alt", "star wars image");
+            // Only taking action if the photo has an appropriate rating
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                // Creating a div for the gif
+                var gifDiv = $("<div>");
 
-          // Prepending the giph to the images div
-          $("#giphs-view").prepend(giph);
+                //add a Bootstrap class to float gifs
+                gifDiv.addClass("float-left");
 
-    //$("#giphs-view").text(JSON.stringify(response));
-    console.log(response);
-});
+                // Storing the result item's rating
+                var rating = results[i].rating;
+
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + rating);
+
+                // Creating an image tag
+                var topicImage = $("<img>");
+
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                topicImage.attr("src", results[i].images.fixed_height.url);
+
+                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                gifDiv.append(p);
+                gifDiv.append(topicImage);
+
+                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                $("#giphs-view").prepend(gifDiv);
+            }
+
+
+            /*           // Saving the image_original_url property
+                      var imageUrl = response.data[i].image_original_url;
+            
+                      // Creating and storing an image tag
+                      var giph = $("<img>");
+            
+                      // Setting the giph src attribute to imageUrl
+                      giph.attr("src", imageUrl);
+                      giph.attr("alt", "star wars image");
+            
+                      // Prepending the giph to the images div
+                      $("#giphs-view").prepend(giph);
+             */
+            //$("#giphs-view").text(JSON.stringify(response));
+            console.log(response.data[i]);
+        }
+    });
 }
 
 // Function for displaying the topic's gif
